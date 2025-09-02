db458ef27c8b3d771c7d32ca3be51a8e66de3f4d
André <145787269+Andreheinzen@users.noreply.github.com>
2025-09-02 14:16:56 -0300

    sdfs

--- main.js
+++ main.js
@@ -77,6 +77,8 @@ function classificaSenha(tamanhoAlfabeto) {
     } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
     }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24))
 }
 
 
